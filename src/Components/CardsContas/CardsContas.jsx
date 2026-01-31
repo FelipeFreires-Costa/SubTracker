b9bb61168507691f7994ec2521073145a8cb3e82
Felipe Freires <felipeplayf13@gmail.com>
2026-01-31 12:31:13 -0300

    botao pagar

--- src/Components/CardsContas/CardsContas.jsx
+++ src/Components/CardsContas/CardsContas.jsx
@@ -2,19 +2,21 @@ import React from 'react'
 import './CardsContas.css'
 import calendario from '../../assets/calendar-lines-svgrepo-com.svg'
 import lixo from '../../assets/trash-xmark-svgrepo-com.svg'
+import check from '../../assets/check-svgrepo-com.svg'
 
 const CardsContas = ({nome, preco, data, aoRemover, situacao, foiPago, pago}) => {
   return (
       <div className={`card ${pago ? "pago" : situacao}`}>
         <div className='infos'>
           <h3>{nome}</h3>
-          <p>R$ {preco.toFixed(2)}</p>
+          <p className='preco'>R$ {preco.toFixed(2)}</p>
           <p><img className='calendario' src={calendario} alt="" /> {data}</p>
           <div className={`detalhes ${situacao}`}>
           <p>{situacao.toUpperCase()}</p>
           </div>
-          <input type="checkbox" onChange={foiPago} checked={pago} />
-          <p>{pago == false ? 'pagar!' : "pago!"}</p>             
+          <button className='pagar' onClick={foiPago}> 
+            <img src={check} alt="" />
+            {pago == false ? 'pagar' : "pago!"}</button>           
           <button className='remover' onClick={aoRemover}>
             <img className='remover-img' src={lixo} alt="" />
           </button>
